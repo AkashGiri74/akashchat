9d1dc9eb12bb838d5799becd978ae4400241e472
AkashGiri74 <155167227+AkashGiri74@users.noreply.github.com>
2025-09-21 15:32:31 +0530

    Fix ALLOWED_HOSTS environment variable parsing
    Removed an extraneous argument from os.getenv in settings.py to correctly parse ALLOWED_HOSTS. Also updated .env to remove trailing comma from ALLOWED_HOSTS value.

--- akashchat_project/settings.py
+++ akashchat_project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-npscph9w038@am04y=$8)))-t%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0','').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
 
 # Application definition
 INSTALLED_APPS = [
